50eb04aa665ddc84e7b92c5dd01eda95808721d0
High School Student <rhs@rlas-macair-33.rlas-116.org>
2018-11-12 10:33:19 -0600

    Initial Commit

---
+++ grade calculator equation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  grade calculator equation
+//
+//  Created by High School Student on 11/12/18.
+//  Copyright © 2018 High School Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
